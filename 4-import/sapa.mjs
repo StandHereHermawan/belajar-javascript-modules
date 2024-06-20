a3ac79f458c972c43e2cac71a7962f6b1aec1164
Arief-Karditya <nolifegansaya@gmail.com>
2024-06-20 17:38:54 +0700

    Import, JS Modules

---
+++ 4-import/sapa.mjs
@@ -0,0 +1,12 @@
+/**
+ * Import Module. JavaScript Module Extension.
+ */
+export function sayHallo(name) {
+    console.info(`Hallo ${name}!`);
+}
+
+export function saySelamatTinggal(name) {
+    console.info(`Selamat Tinggal ${name}!`);
+}
+
+export const nameHilmi = "Hilmi Akbar Muharrom";
