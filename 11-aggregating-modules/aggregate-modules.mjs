3f9bf2643bd2869d7044c07f024b896b4c92939e
Arief-Karditya <nolifegansaya@gmail.com>
2024-06-21 23:38:51 +0700

    Aggregating Modules, JS Modules

---
+++ 11-aggregating-modules/aggregate-modules.mjs
@@ -0,0 +1,4 @@
+export { Person } from "/11-aggregating-modules/class.mjs";
+export { sayHello, sayGoodBye } from "/11-aggregating-modules/function.mjs";
+export { nameVar } from "/11-aggregating-modules/variable.mjs";
+export { sum } from "/11-aggregating-modules/multiple.mjs";
---
+++ 11-aggregating-modules/multiple.mjs
@@ -0,0 +1,17 @@
+/**
+ * Export Multiple
+ */
+const company = "Belum Ada .Tbk";
+
+function sum(first, second) {
+    return first + second;
+}
+
+class Company {
+    constructor(lokasi, nama) {
+        this.lokasi = lokasi;
+        this.nama = nama;
+    }
+}
+
+export { company, sum, Company };
